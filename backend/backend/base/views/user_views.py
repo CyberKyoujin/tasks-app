c15470d6c30aacc40be4307b78ea5cd6b9d0c6a5
CyberKyoujin <101016388+CyberKyoujin@users.noreply.github.com>
2024-11-17 22:28:24 +0100

    Update user_views.py

--- backend/backend/base/views/user_views.py
+++ backend/backend/base/views/user_views.py
@@ -21,4 +21,3 @@ class UserRegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class 
