65163b9f246cef2b6cb95e15fb281eaf435c8dc5
CyberKyoujin <cyber.kyoujin@gmail.com>
2024-11-17 22:38:00 +0100

    Updating

--- backend/backend/base/views/user_views.py
+++ backend/backend/base/views/user_views.py
@@ -20,5 +20,3 @@ class UserRegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class 
